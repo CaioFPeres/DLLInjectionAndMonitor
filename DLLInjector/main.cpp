acdcbeacacb768c23085c3a39f2677336675b088
Caio Peres <caiofperes@hotmail.com>
2023-02-04 20:17:40 -0300

    Fixed a string

--- DLLInjector/main.cpp
+++ DLLInjector/main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char* argv[])
 {
 
     if (argc != 2) {
-        cout << "Usage: packetlogger processName" << endl;
+        cout << "Usage: DLLInjector.exe processName.exe" << endl;
         return 1;
     }
 
